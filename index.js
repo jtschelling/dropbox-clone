28fe4571099deb524ea5b74473c802532b4729de
JT Schelling <jtschelling@wisc.edu>
2018-06-05 22:20:26 -0500

    testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = process.argv[2];
+const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
