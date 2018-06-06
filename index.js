be834f859fe520d94f2a2eb4f2b2a17f0e1f0764
JT Schelling <jtschelling@wisc.edu>
2018-06-05 22:25:58 -0500

    testing

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const http = require('http');
-const hostname = 'jtschelling-testing.herokuapp.com';
 const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on ${port}/`);
 });
