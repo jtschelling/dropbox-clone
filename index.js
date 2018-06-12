271fb92572162739d30da8d7142b4ad8835fa63d
JT Schelling <jtschelling@wisc.edu>
2018-06-11 19:55:07 -0500

    changed response message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('testing new message\n');
+  res.end('another message\n');
 });
 server.listen(port, () => {
   // eslint-disable-next-line
